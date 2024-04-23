d04fcaf0fad1b0e4094d5b4d38bb65424b7fd346
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-04-23 20:23:51 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Schaltwert += -5
+    basic.showNumber(Schaltwert)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    Schaltwert += 5
+    basic.showNumber(Schaltwert)
+})
+let Lichtschranke = 0
+let Schaltwert = 0
+basic.showString("calliope-net/ft2-trockner-61")
+let o4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
+pins.digitalWritePin(DigitalPin.P0, 1)
+Schaltwert = 60
+loops.everyInterval(400, function () {
+    Lichtschranke = pins.analogReadPin(AnalogPin.P2)
+    o4digit.show(Lichtschranke)
+    if (Lichtschranke < Schaltwert) {
+        basic.setLedColor(0xff0000)
+        motors.motorCommand(MotorCommand.Break)
+    } else {
+        basic.setLedColor(0x00ff00)
+        motors.motorPower(95)
+    }
+})
